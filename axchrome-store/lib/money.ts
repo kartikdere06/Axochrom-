64aed5c7889000ca667022abe451a244267d1577
KARTIK DERE <kartikdere06@gmail.com>
2026-09-13 11:11:05 +0530

    Delete axchrome-store directory

--- axchrome-store/lib/money.ts
+++
@@ -1,6 +0,0 @@
-export function formatCents(cents: number, currency: string = "INR") {
-  return new Intl.NumberFormat("en-IN", {
-    style: "currency",
-    currency,
-  }).format(cents / 100);
-}
